5cc191e853f4e230a26cb5a7280dfdf05f15a2f8
Matthias <Matthias@Workstation>
2016-02-15 12:24:57 +0100

    removed unnecessary casts

--- src/sorting_algorithms/Quicksort.java
+++ src/sorting_algorithms/Quicksort.java
@@ -25,24 +25,17 @@ import algo_interfaces.InPlaceSort;
  */
 public class Quicksort implements InPlaceSort {
 
-    /**
-     * A reference to the array that will be sorted
-     */
-    private Object[] _values;
-
     @Override
     public <T> void sort(T[] values) {
         if (values.length > 1) {
-            _values = values;
-            quicksortComparable(0, _values.length - 1);
+            quicksortComparable(0, values.length - 1, values);
         }
     }
 
     @Override
     public <T> void sort(T[] values, Comparator<? super T> c) {
         if (values.length > 1) {
-            _values = values;
-            quicksortUsingComparator(0, _values.length - 1, c);
+            quicksortUsingComparator(0, values.length - 1, c, values);
         }
     }
 
@@ -55,11 +48,11 @@ public class Quicksort implements InPlaceSort {
      * @param right The right boundary of the array
      * @param c The comparator used for sorting the array
      */
-    private <T> void quicksortUsingComparator(int left, int right, Comparator<? super T> c) {
+    private <T> void quicksortUsingComparator(int left, int right, Comparator<? super T> c, T[] values) {
         if (left < right) {
-            int div = divideUsingComparator(left, right, c);
-            quicksortUsingComparator(left, div - 1, c);
-            quicksortUsingComparator(div + 1, right, c);
+            int div = divideUsingComparator(left, right, c, values);
+            quicksortUsingComparator(left, div - 1, c, values);
+            quicksortUsingComparator(div + 1, right, c, values);
         }
     }
 
@@ -67,14 +60,15 @@ public class Quicksort implements InPlaceSort {
      * Main method for recursive call of Quicksort-algorithm using the natural
      * ordering of the array elements for sorting
      *
+     * @param <T> Generic type parameter
      * @param left The left boundary of the array
      * @param right The right boundary of the array
      */
-    private void quicksortComparable(int left, int right) {
+    private <T> void quicksortComparable(int left, int right, T[] values) {
         if (left < right) {
-            int div = divideComparable(left, right);
-            quicksortComparable(left, div - 1);
-            quicksortComparable(div + 1, right);
+            int div = divideComparable(left, right, values);
+            quicksortComparable(left, div - 1, values);
+            quicksortComparable(div + 1, right, values);
         }
     }
 
@@ -87,35 +81,34 @@ public class Quicksort implements InPlaceSort {
      * @param c The comparator used for sorting the array
      * @return The new position of the pivot element
      */
-    @SuppressWarnings("unchecked")
-    private <T> int divideUsingComparator(int left, int right, Comparator<? super T> c) {
+    private <T> int divideUsingComparator(int left, int right, Comparator<? super T> c, T[] values) {
         int i = left;
         int j = right - 1; //element to the left of pivot
-        T pivot = (T) _values[right];
+        T pivot = values[right];
 
         do {
             /*search element from the left, which is bigger than pivot*/
-            while (c.compare((T) _values[i], pivot) <= 0 && i < right) {
+            while (c.compare(values[i], pivot) <= 0 && i < right) {
                 ++i;
             }
 
             /*search element from the right, which is smaller than pivot*/
-            while (c.compare((T) _values[j], pivot) >= 0 && j > left) {
+            while (c.compare(values[j], pivot) >= 0 && j > left) {
                 --j;
             }
 
             if (i < j) { //swap
-                T temp = (T) _values[i];
-                _values[i] = _values[j];
-                _values[j] = temp;
+                T temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
             }
         } while (i < j);
 
         /*swap pivot with new final position*/
-        if (c.compare((T) _values[i], pivot) > 0) {
-            T temp = (T) _values[i];
-            _values[i] = _values[right];
-            _values[right] = temp;
+        if (c.compare(values[i], pivot) > 0) {
+            T temp = values[i];
+            values[i] = values[right];
+            values[right] = temp;
         }
 
         return i; //return position of pivot
@@ -130,36 +123,36 @@ public class Quicksort implements InPlaceSort {
      * @return The new position of the pivot element
      */
     @SuppressWarnings("unchecked")
-    private <T> int divideComparable(int left, int right) {
+    private <T> int divideComparable(int left, int right, T[] values) {
         int i = left;
         int j = right - 1; //element to the left of pivot
-        T pivot = (T) _values[right];
+        T pivot = values[right];
 
         do {
             /*search element from the left, which is bigger than pivot*/
-            while (((Comparable<? super T>) _values[i]).compareTo(pivot) <= 0
+            while (((Comparable<? super T>) values[i]).compareTo(pivot) <= 0
                     && i < right) {
                 ++i;
             }
 
             /*search element from the right, which is smaller than pivot*/
-            while (((Comparable<? super T>) _values[j]).compareTo(pivot) >= 0
+            while (((Comparable<? super T>) values[j]).compareTo(pivot) >= 0
                     && j > left) {
                 --j;
             }
 
             if (i < j) { //swap
-                T temp = (T) _values[i];
-                _values[i] = _values[j];
-                _values[j] = temp;
+                T temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
             }
         } while (i < j);
 
         /*swap pivot with new final position*/
-        if (((Comparable<? super T>) _values[i]).compareTo(pivot) > 0) {
-            T temp = (T) _values[i];
-            _values[i] = _values[right];
-            _values[right] = temp;
+        if (((Comparable<? super T>) values[i]).compareTo(pivot) > 0) {
+            T temp = values[i];
+            values[i] = values[right];
+            values[right] = temp;
         }
 
         return i; //return position of pivot
