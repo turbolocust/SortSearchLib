84295e847e55dfb1d3303f078ee0d75300219bf7
Matthias Fussenegger <matfu2@gmail.com>
2017-07-18 19:32:38 +0200

    Updated license headers

--- test/org/sort_search_lib/java/sort/QuicksortTest.java
+++ test/org/sort_search_lib/java/sort/QuicksortTest.java
@@ -1,18 +1,25 @@
 /*
- * Copyright (C) 2016 Matthias Fussenegger
+ * The MIT License
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright 2017 Matthias Fussenegger
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.sort_search_lib.java.sort;
 
@@ -68,13 +75,12 @@ public class QuicksortTest implements TestableSort {
      * Test of sort method, of class Quicksort.
      */
     @Test
-    @SuppressWarnings("unchecked")
     @Override
     public void testSort_Comparator() {
         System.out.println("sort using comparator - Quicksort");
         Quicksort instance = new Quicksort();
         Object[] values = VALUES.toArray();
-        Comparator c = (Comparator) (Object o1, Object o2) -> {
+        Comparator<Object> c = (Comparator<Object>) (Object o1, Object o2) -> {
             Integer v1 = (Integer) o1;
             Integer v2 = (Integer) o2;
             return v2.compareTo(v1);
